902dd41002b81480de518ac71a0890cccd65c8b5
kangarko <matej@matejpacan.com>
2025-10-01 18:47:01 +0200

    Refactor and reformat code

--- src/main/java/org/mineacademy/boss/hook/ScaleTrait.java
+++ src/main/java/org/mineacademy/boss/hook/ScaleTrait.java
@@ -1,64 +1,63 @@
 package org.mineacademy.boss.hook;
 
-import net.citizensnpcs.api.CitizensAPI;
-import net.citizensnpcs.api.npc.NPC;
-import net.citizensnpcs.api.npc.NPCRegistry;
-import net.citizensnpcs.api.persistence.Persist;
-import net.citizensnpcs.api.trait.Trait;
-import net.citizensnpcs.api.trait.TraitName;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.mineacademy.boss.model.Boss;
 import org.mineacademy.boss.model.SpawnedBoss;
 import org.mineacademy.fo.remain.CompAttribute;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.npc.NPCRegistry;
+import net.citizensnpcs.api.persistence.Persist;
+import net.citizensnpcs.api.trait.Trait;
+import net.citizensnpcs.api.trait.TraitName;
+
 @TraitName("ma_scaletrait")
 public final class ScaleTrait extends Trait {
 
-    /**
-     * Stores last known current health since Citizens does not save this value
-     */
-    @Persist("boss_health")
-    double scale = -1;
+	/**
+	 * Stores last known current health since Citizens does not save this value
+	 */
+	@Persist("boss_health")
+	double scale = -1;
 
-    public ScaleTrait() {
-        super("ma_scaletrait");
-    }
+	public ScaleTrait() {
+		super("ma_scaletrait");
+	}
 
-    /*
-     * Update last known health
-     */
-    public static void updateScale(Entity entity, double scale) {
-        if (scale < 0) scale = -1;
+	/*
+	 * Update last known health
+	 */
+	public static void updateScale(Entity entity, double scale) {
+		if (scale < 0)
+			scale = -1;
 
-        final SpawnedBoss boss = Boss.findBoss(entity);
+		final SpawnedBoss boss = Boss.findBoss(entity);
 
-        if (boss != null) {
-            final LivingEntity living = boss.getEntity();
+		if (boss != null) {
+			final NPCRegistry registry = CitizensAPI.getNPCRegistry();
+			final NPC npc = registry.getNPC(entity);
 
-            final NPCRegistry registry = CitizensAPI.getNPCRegistry();
+			if (npc != null)
+				npc.getTraitNullable(ScaleTrait.class).scale = scale;
+		}
+	}
 
-            final NPC npc = registry.getNPC(entity);
+	/**
+	 * Sets the health back instead of letting Citizens reset it to max if we saved it
+	 */
+	@Override
+	public void onSpawn() {
+		if (!this.npc.isSpawned())
+			return;
 
-            if (npc != null)
-                npc.getTraitNullable(ScaleTrait.class).scale = scale;
-        }
-    }
+		final LivingEntity entity = (LivingEntity) this.npc.getEntity();
+		final SpawnedBoss spawnedBoss = Boss.findBoss(entity);
 
-    /**
-     * Sets the health back instead of letting Citizens reset it to max if we saved it
-     */
-    @Override
-    public void onSpawn() {
-        if (!this.npc.isSpawned())
-            return;
-
-        final LivingEntity entity = (LivingEntity) this.npc.getEntity();
-        final SpawnedBoss spawnedBoss = Boss.findBoss(entity);
-
-        if (spawnedBoss != null) {
-            if (this.scale > 0)
-                CompAttribute.SCALE.set(entity, this.scale);
-        }
-    }
+		if (spawnedBoss != null) {
+			if (this.scale > 0)
+				CompAttribute.SCALE.set(entity, this.scale);
+		}
+	}
 }
